c20642b020874d01c6530d2a413bfe74916c516f
AungTayZarMaung25 <aungtayzarmaung.mhs@gmail.com>
2020-10-14 19:53:47 +0630

    Updates

---
+++ precache-manifest.52a1a70d7f15dfb04c26a8fac384e4d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5198d3cc57181066c59d3899a83b953",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e20ad9f38eeb362c9705",
+    "url": "/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "fa9eaf8a677bcd234f93",
+    "url": "/static/js/2.57b8d150.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.57b8d150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20ad9f38eeb362c9705",
+    "url": "/static/js/main.a25e72e6.chunk.js"
+  },
+  {
+    "revision": "d8111fa043255159b8a8",
+    "url": "/static/js/runtime-main.a5025baa.js"
+  }
+]);
